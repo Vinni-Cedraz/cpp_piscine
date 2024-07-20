59253dce1ca921494ea0d276eadab8ce334e69b9
Vinni-Cedraz <planetexpress0101@gmail.com>
2024-07-20 06:56:02 -0300

    small refactor

--- day06/ex00/ScalarConverter.cpp
+++ day06/ex00/ScalarConverter.cpp
@@ -4,48 +4,56 @@
 #include <sstream>
 #include <string>
 
-void ScalarConverter::convert(const std::string &str) {
+void convertInt(const std::string &str) {
+    int i;
     std::istringstream iss(str);
 
-    int i;
-    if (iss >> i) {
+    if (iss >> i)
         std::cout << "int: " << i << std::endl;
-    } else {
+    else
         std::cout << "int: invalid" << std::endl;
-    }
+}
 
-    iss.clear();
-    iss.str(str);
-
+void convertFloat(const std::string &str) {
     float f;
-    if (iss >> f) {
+    std::istringstream iss(str);
+    if (iss >> f)
         std::cout << "float: " << f << std::endl;
-    } else {
+    else
         std::cout << "float: invalid" << std::endl;
-    }
+}
 
-    iss.clear();
-    iss.str(str);
-
+void convertDouble(const std::string &str) {
     double d;
-    if (iss >> d) {
-        std::cout << "double: " << d << std::endl;
-    } else {
-        std::cout << "double: invalid" << std::endl;
-    }
+    std::istringstream iss(str);
+    if (iss >> d)
+        std::cout << "float: " << d << std::endl;
+    else
+        std::cout << "float: invalid" << std::endl;
+}
 
+void convertChar(const std::string &str) {
+    std::istringstream iss(str);
+    int i;
     if (str.length() == 1) {
         char c = str[0];
-        std::cout << "char: " << c << std::endl;
+        std::cout << "char: '" << c << "'" << std::endl;
     } else {
         iss.clear();
         iss.str(str);
 
         if (iss >> i && i >= CHAR_MIN && i <= CHAR_MAX) {
             char c = static_cast<char>(i);
-            std::cout << "char: " << c << std::endl;
+            std::cout << "char: '" << c << '\''<< std::endl;
         } else {
             std::cout << "char: out of range" << std::endl;
         }
     }
+}
+
+void ScalarConverter::convert(const std::string &str) {
+    convertInt(str);
+    convertFloat(str);
+    convertDouble(str);
+	convertChar(str);
 }
