af2dca6e82a5ca9453ccd04a06bbb160bab4758f
Vinni-Cedraz <planetexpress0101@gmail.com>
2024-07-24 14:02:51 -0300

    ex02

--- day07/ex00/whatever.hpp
+++ day07/ex00/whatever.hpp
@@ -1,3 +1,5 @@
+#pragma once
+
 template <typename type> type min(type a, type b) { return a < b ? a : b; }
 template <typename type> type max(type a, type b) { return a > b ? a : b; }
 template <typename type> void swap(type &a, type &b) {
---
+++ day07/ex02/Array.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+template <typename T>
+class Array {
+ private:
+  T *_array;
+  unsigned int _size;
+
+ public:
+  Array() : _array(), _size() {}
+  Array(unsigned int size) : _array(), _size(size) {
+    if (size) _array = new T[size];
+  }
+  Array(const Array &other) { *this = Array(other._size); }
+  Array &operator=(const Array &other) {
+  		if (this != &other) {
+  			delete[] _array;
+  			_size = other._size;
+  			_array = new T[_size];
+  			for (unsigned int i = 0; i < _size; i++) _array[i] = other._array[i];
+  		}
+  		return *this;
+  	}
+};
