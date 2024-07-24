ef7ecc972cfa0cc90477d4fca7a9ba06bd0ad203
Vinni-Cedraz <planetexpress0101@gmail.com>
2024-07-24 14:36:19 -0300

    ex02 started

--- day07/ex02/Array.hpp
+++ day07/ex02/Array.hpp
@@ -13,12 +13,12 @@ class Array {
   }
   Array(const Array &other) { *this = Array(other._size); }
   Array &operator=(const Array &other) {
-  		if (this != &other) {
-  			delete[] _array;
-  			_size = other._size;
-  			_array = new T[_size];
-  			for (unsigned int i = 0; i < _size; i++) _array[i] = other._array[i];
-  		}
-  		return *this;
-  	}
+    if (this != &other) {
+      _size = other._size;
+      for (unsigned int i = 0; i < _size; i++) {
+        _array[i] = other._array[i];
+      }
+    }
+    return *this;
+  }
 };
