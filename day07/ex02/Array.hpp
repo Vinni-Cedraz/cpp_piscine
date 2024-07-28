dd84c1e00868aa64f482f7e3bea06c241d408175
Vinni-Cedraz <planetexpress0101@gmail.com>
2024-07-28 20:18:53 -0300

    fixed

--- day07/ex02/Array.hpp
+++ day07/ex02/Array.hpp
@@ -15,13 +15,13 @@ public:
   }
   Array(const Array &other) : _array(), _size(other._size) {
     if (this != &other && _size) {
-      _array = new T[_size];
       *this = other;
     }
   }
   // operators:
   Array &operator=(const Array &other) {
-    _size = other._size;
+	this->~Array();
+	new(this) Array(other._size);
     for (unsigned int i = 0; i < _size; i++) {
       _array[i] = other._array[i];
     }
--- day07/ex02/main.cpp
+++ day07/ex02/main.cpp
@@ -1,25 +1,28 @@
+#include "Array.hpp"
 #include <cstdlib>
 #include <iostream>
 #include <sstream>
-#include "Array.hpp"
+#define CYAN "\033[36m"
+#define RESET "\033[0m"
 
-#define MAX_VAL 15
-int main(int, char**) {
+#define MAX_VAL 5
+int main(int, char **) {
+
+  std::cout << CYAN "Proving that it works just like an array of ints:\n" RESET;
   Array<int> my_array_class(MAX_VAL);
-  int* mirror = new int[MAX_VAL];
+  int *default_array = new int[MAX_VAL];
   srand(time(NULL));
   // TESTING [] OPERATOR:
   for (int i = 0; i < MAX_VAL; i++) {
-    const int value = rand();
+    const int value = rand() % 50;
     my_array_class[i] = value;
-    mirror[i] = value;
+    default_array[i] = value;
   }
   for (int i = 0; i < MAX_VAL; i++) {
-    if (mirror[i] != my_array_class[i]) {
-      std::cerr << "didn't save the same value!!" << std::endl;
-      return 1;
-    }
+    std::cout << "my_array_class[" << i << "] = " << my_array_class[i] << std::endl;
+    std::cout << "default_array[" << i << "] = " << default_array[i] << std::endl;
   }
+  delete[] default_array;
 
   // TESTING COPY CONSTRUCTOR AND ASSIGNMENT OPERATOR:
   {
@@ -27,17 +30,19 @@ int main(int, char**) {
     Array<int> test(tmp);
   }
 
+  std::cout << CYAN "trying to access indexes out of range should print exceptions:\n" RESET;
   try {
     my_array_class[-2] = 0;
-  } catch (const std::exception& e) {
+  } catch (const std::exception &e) {
     std::cerr << e.what() << '\n';
   }
   try {
     my_array_class[MAX_VAL] = 0;
-  } catch (const std::exception& e) {
+  } catch (const std::exception &e) {
     std::cerr << e.what() << '\n';
   }
 
+  std::cout << CYAN "Proving that the Array template class also works for strings:" RESET << std::endl;
   Array<std::string> my_array_class_strings(MAX_VAL);
   for (int i = 0; i < MAX_VAL; i++) {
     std::stringstream index;
@@ -47,5 +52,18 @@ int main(int, char**) {
   for (int i = 0; i < MAX_VAL; i++) {
     std::cout << my_array_class_strings[i] << std::endl;
   }
-  delete[] mirror;  //
+
+  std::cout << CYAN "Overwritting an Array a(5) with an Array b(10) of different size:\n" RESET;
+  Array<int> a(5);
+  for (int i = 0; i != 5; i++) {
+    a[i] = i;
+  }
+
+  Array<int> b(10);
+  for (int i = 0; i != 10; i++) {
+    b[i] = i;
+  }
+
+  a = b;
+  std::cout << "Array a size should now be 10: " << a.size() << std::endl;
 }
