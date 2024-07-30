41dc5cb2cd882a6ff1add109bf13c3a199ca0142
Vinni-Cedraz <planetexpress0101@gmail.com>
2024-07-30 11:33:19 -0300

    ex00 done

---
+++ day08/ex00/easyfind.hpp
@@ -0,0 +1,53 @@
+#pragma once
+#include <algorithm>
+#include <deque>
+#include <iostream>
+#include <list>
+#include <queue>
+#define CYAN "\033[0;36m"
+#define RED "\033[1;31m"
+#define RESET "\033[0m"
+
+template <typename T>
+typename T::iterator easyfind(T &container, int b) {
+  const typename T::iterator it = std::find(container.begin(), container.end(), b);
+  if (it == container.end()) {
+    std::cerr << RED "Element Not Found!" RESET << std::endl;
+  } else {
+    std::cout << CYAN "Element Found: \n" RESET;
+    std::cout << "map[" << std::distance(container.begin(), it) << "] ==> " << *it << std::endl;
+  }
+  return it;
+}
+
+template <typename T>
+std::ostream &operator<<(std::ostream &os, const std::vector<T> &vec) {
+  for (typename std::vector<T>::const_iterator i = vec.begin(); i != vec.end(); ++i) {
+    os << "["<< std::distance(vec.begin(), i) << "] " << *i << std::endl;
+  }
+  return os;
+}
+
+template <typename T>
+std::ostream &operator<<(std::ostream &os, const std::list<T> &vec) {
+  for (typename std::vector<T>::const_iterator i = vec.begin(); i != vec.end(); ++i) {
+    os << "["<< std::distance(vec.begin(), i) << "] " << *i << std::endl;
+  }
+  return os;
+}
+
+template <typename T>
+std::ostream &operator<<(std::ostream &os, const std::queue<T> &vec) {
+  for (typename std::vector<T>::const_iterator i = vec.begin(); i != vec.end(); ++i) {
+    os << "["<< std::distance(vec.begin(), i) << "] " << *i << std::endl;
+  }
+  return os;
+}
+
+template <typename T>
+std::ostream &operator<<(std::ostream &os, const std::deque<T> &vec) {
+  for (typename std::vector<T>::const_iterator i = vec.begin(); i != vec.end(); ++i) {
+    os << "["<< std::distance(vec.begin(), i) << "] " << *i << std::endl;
+  }
+  return os;
+}
---
+++ day08/ex00/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+
+#include "easyfind.hpp"
+
+int main() {
+  std::vector<int> map;
+
+  for (int i = 0; i < 10; i++) {
+    map.push_back(i);
+  }
+  map.push_back(42);
+  for (int i = 21; i < 31; i++) {
+    map.push_back(i);
+  }
+  std::cout << CYAN "This is the vector of ints" RESET << std::endl;
+  std::cout << map << std::endl;
+  std::vector<int>::iterator i;
+  std::cout << CYAN "Looking for 42: " RESET << std::endl;
+  i = easyfind(map, 42);
+  std::cout << CYAN "Looking for 43: " RESET << std::endl;
+  i = easyfind(map, 43);
+  (void)i;
+}
