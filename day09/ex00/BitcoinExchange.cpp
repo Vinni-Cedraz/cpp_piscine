9149f34ce8196624adbceaf4500de07812fceebb
Vinni-Cedraz <planetexpress0101@gmail.com>
2024-10-11 11:56:44 -0300

    transforming .csv and .txt into data structures

--- day09/ex00/BitcoinExchange.cpp
+++ day09/ex00/BitcoinExchange.cpp
@@ -1,147 +1,15 @@
 #include "BitcoinExchange.hpp"
-#include <algorithm>
-#include <iostream>
-#include <sstream>
 
-BitcoinExchange::BitcoinExchange(void) {
-  std::cout << "BitcoinExchange class default constructor called." << std::endl;
-};
-
-BitcoinExchange::BitcoinExchange(const BitcoinExchange &BitcoinExchange) {
-  std::cout << "BitcoinExchange class copy constructor called." << std::endl;
-  *this = BitcoinExchange;
-};
-
-BitcoinExchange &BitcoinExchange::operator=(const BitcoinExchange &rhs) {
-  std::cout << "BitcoinExchange class copy assign operator called." << std::endl;
-  if (this != &rhs) {
-    *this = rhs;
+std::ostream &operator<<(std::ostream &os, const std::map<std::string, float> &map) {
+  for (std::map<std::string, float>::const_iterator it = map.begin(); it != map.end(); ++it) {
+    os << "Key: " << it->first << ", Value: " << it->second << std::endl;
   }
-  return *this;
-};
-
-BitcoinExchange::~BitcoinExchange(void) { std::cout << "BitcoinExchange class destructor called." << std::endl; };
-
-void BitcoinExchange::_findNearMatch(std::string line) {
-  std::stringstream ss(line);
-  std::string timeStamp;
-  std::string amount;
-  double dAmount;
-
-  if (std::getline(ss, timeStamp, '|') && std::getline(ss, amount)) {
-    timeStamp.erase(timeStamp.length() - 1);
-    std::stringstream dSSAmount(amount);
-    dSSAmount >> dAmount;
-    if (dSSAmount.fail())
-      errorln("Error: " << amount << " is not a valid number.");
-  }
-  std::map<std::string, double>::iterator iter = this->_btcDb.find(timeStamp);
-  if (iter != this->_btcDb.end()) {
-    println(GREEN << timeStamp << " => " << dAmount << " = " << dAmount * iter->second);
-  } else {
-    std::map<std::string, double>::iterator rIter = this->_btcDb.end();
-    rIter--;
-    std::cout << std::fixed;
-    std::cout.precision(2);
-    while (rIter != this->_btcDb.begin()) {
-      if (timeStamp < rIter->first) {
-        rIter--;
-      } else {
-        println(GREEN << timeStamp << " => " << dAmount << " = " << dAmount * rIter->second);
-        return;
-      }
-    };
-  }
+  return os;
 }
 
-void BitcoinExchange::_validateAmount(std::string line) {
-  size_t pos;
-  double value;
-
-  pos = line.find("|");
-  value = std::atof(line.substr(pos + 2).c_str());
-  if (value < 0) {
-    errorln("Error: not a positive number.");
-  } else if (value >= 1000) {
-    errorln("Error: too large a number.");
-  } else {
-    this->_findNearMatch(line);
+std::ostream &operator<<(std::ostream &os, const std::list<std::pair<std::string, float> > &lst) {
+  for (std::list<std::pair<std::string, float> >::const_iterator it = lst.begin(); it != lst.end(); ++it) {
+    os << "Key: " << it->first << ", Value: " << it->second << std::endl;
   }
-};
-
-void BitcoinExchange::_timestampChecker(std::string line) {
-  std::string timestamp = line.substr(0, 10);
-  std::string baseDate = "2009-01-02";
-
-  if (timestamp < baseDate || timestamp.length() != 10 || std::count(timestamp.begin(), timestamp.end(), '-') != 2 ||
-      atoi(timestamp.substr(5, 2).c_str()) > 12 || atoi(timestamp.substr(8, 2).c_str()) > 31) {
-    std::cerr << RED << "Error: bad input => ";
-    errorln(line);
-  } else {
-    this->_validateAmount(line);
-  }
-};
-
-void BitcoinExchange::_formatChecker(std::string line) {
-  if (line.find("|") == std::string::npos) {
-    std::cerr << RED << "Error: bad input => ";
-    errorln(line);
-  } else {
-    this->_timestampChecker(line);
-  }
-};
-
-void BitcoinExchange::_inputExecutor(void) {
-  std::string buffer;
-
-  std::getline(this->_inputFile, buffer);
-  while (std::getline(this->_inputFile, buffer)) {
-    this->_formatChecker(buffer);
-  }
-};
-
-void BitcoinExchange::_initDb(void) {
-  std::ifstream csvFile;
-  std::string test;
-
-  csvFile.open("data.csv");
-  while (std::getline(csvFile, test)) {
-    std::istringstream iss(test);
-    std::string date;
-    std::string amount;
-
-    if (std::getline(iss, date, ',') && std::getline(iss, amount)) {
-      double dAmount;
-      std::stringstream valueStream(amount);
-      valueStream >> dAmount;
-      if (!valueStream.fail())
-        this->_btcDb[date] = dAmount;
-    }
-  }
-  csvFile.close();
+  return os;
 }
-
-void BitcoinExchange::_inputValidator(char *inputPath) {
-  std::ifstream inputFile;
-
-  inputFile.open(inputPath);
-  if (inputFile.fail())
-    throw FilePathNotFoundException();
-  this->_inputFile.open(inputPath);
-};
-
-void BitcoinExchange::runExchange(char *inputPath) {
-  try {
-    this->_inputValidator(inputPath);
-    this->_initDb();
-    this->_inputExecutor();
-
-  } catch (BitcoinExchange::FilePathNotFoundException &e) {
-    errorln(e.what());
-    return;
-  }
-};
-
-const char *BitcoinExchange::FilePathNotFoundException::what() const throw() {
-  return ("Fail opening the file, path not found.");
-};
--- day09/ex00/main.cpp
+++ day09/ex00/main.cpp
@@ -1,19 +1,86 @@
 #include "BitcoinExchange.hpp"
-#include <iostream>
 
-int main(int argc, char**argv)
-{
-	if (argc < 2) {
-		errorln("Error: could not open file.");
-		return (0);
-	}
-	else if (argc > 2) {
-		errorln("Error: too many arguments.");
-		println(CYAN << "Usage: ./btc input.txt");
-	}
+static std::fstream *open_file(const char *filename) {
+  std::fstream *file = new std::fstream;
+  file->open(filename, std::ios::in);
+  if (!*file) {
+    std::cerr << "Error: Unable to open file :" << filename << std::endl;
+    exit(1);
+  }
+  return file;
+}
 
-	BitcoinExchange	exchange;
-	
-	exchange.runExchange(argv[1]);
-	return (0);
+static std::map<std::string, float> create_database(const char *filename) {
+  std::fstream *file = open_file(filename);
+  std::map<std::string, float> database;
+  std::string line;
+  // Skip the first line
+  std::getline(*file, line);
+
+  while (std::getline(*file, line)) {
+    std::istringstream iss(line);
+    std::string date;
+    float value;
+
+    // Try to read the date
+    std::getline(iss, date, ',');
+
+    // Try to read the value
+    iss >> value;
+
+    // Add the date and value to the map
+    database[date] = value;
+  }
+  delete file;
+  return database;
+}
+
+static std::list<std::pair<std::string, float> > create_database_list(const char *filename) {
+  std::fstream *file = open_file(filename);
+  std::list<std::pair<std::string, float> > database;
+  std::string line;
+  // Skip the first line
+  std::getline(*file, line);
+
+  while (std::getline(*file, line)) {
+    std::istringstream iss(line);
+    std::string date;
+    float value;
+    std::string tmp;
+
+    // Try to read the date
+    std::getline(iss, date, ' ');
+    std::getline(iss, tmp, '|');
+    std::getline(iss, tmp, ' ');
+
+    // Try to read the value
+    iss >> value;
+
+    // Add the date and value to the list
+    database.push_back(std::make_pair(date, value));
+  }
+  delete file;
+  return database;
+}
+
+int main(int argc, char **argv) {
+  if (argc < 2) {
+    std::cout << "Error: missing input argument." << std::endl;
+    return (1);
+  } else if (argc > 2) {
+    std::cout << "Error: too many arguments." << std::endl;
+    std::cout << "Usage: ./btc input.txt" << std::endl;
+    return (1);
+  }
+  (void)argv;
+
+  // open file
+  std::map<std::string, float> csv = create_database("data.csv");
+  std::list<std::pair<std::string, float> > input = create_database_list(argv[1]);
+  std::cout << csv << std::endl;
+  std::cout << input << std::endl;
+
+  // BitcoinExchange exchange;
+  //
+  // exchange.runExchange(file);
 }
